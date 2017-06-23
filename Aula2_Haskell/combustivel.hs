bac4f7a5fa38d7f9d36a7d45a6413e985b17e636
SuelanyBr <maria.cruz@ccc.ufcg.edu.br>
2017-06-23 15:42:46 -0300

    Aula 2 - combustivel

--- Aula2_Haskell/combustivel.hs
+++ Aula2_Haskell/combustivel.hs
@@ -1,7 +1,7 @@
-preco_alcool :: Int -> Double -> Double
+preco_alcool :: Double -> Double -> Double
 preco_alcool x y = x*y
 
-preco_gasolina :: Int -> Double -> Double
+preco_gasolina :: Double -> Double -> Double
 preco_gasolina x y = x*y
 
 guarda :: Double -> Double -> Double
@@ -10,13 +10,16 @@ guarda alcool gasolina
 	| (alcool < (gasolina * 0.7)) = alcool
 
 main = do
-	x <- getline
-	y <- getline
-	qtde_litros <- getline
+	x <- getLine
+	y <- getLine
+	z <- getLine
 	
+	let alcool = (read(x) :: Double)
+	let gasolina = (read(y) :: Double)
+	let qtde = (read(z) :: Double)
 	
-	total_alcool = preco_alcool qtde_litros x 
-	total_gasolina = preco_gasolina qtde_litros y
+	let total_alcool = preco_alcool qtde alcool 
+	let total_gasolina = preco_gasolina qtde gasolina
 	
 	print (guarda total_alcool total_gasolina)
 	
